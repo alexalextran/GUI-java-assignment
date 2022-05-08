0ae22bc35f97d7fb8dfb5248f4132b09856a941e
Alex Tran <alextran8385@gmail.com>
2022-05-09 07:35:05 +1000

    add page

--- src/controller/TMSController.java
+++ src/controller/TMSController.java
@@ -1,8 +1,11 @@
 package controller;
 
 import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 import model.Faculty;
 import model.Student;
 import model.Students;
@@ -15,10 +18,20 @@ import model.Students;
 public class TMSController extends Controller<Faculty> {
     @FXML private TableView<Student> studentsTv;
     
+    
+  public final Student getSelectedStudent(){
+   return studentsTv.getSelectionModel().getSelectedItem();   
+  }
+    
    public final Faculty getFaculty(){
     return model;
 }
    
-  
-   
+ @FXML private void handleSelectStudent(ActionEvent event){
+     Student student = getSelectedStudent();
+ }
+ 
+ @FXML private void handleAddStudent(ActionEvent event) throws Exception{
+     ViewLoader.showStage(getFaculty(), "/view/student.fxml", "Adding new student", new Stage());
+ }
 }
